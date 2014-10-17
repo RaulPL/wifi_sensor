985d01eb239f1915519669436c6e002ef9bebde2
Raul Peralta Lozada <raulpl25@gmail.com>
2014-10-16 23:50:27 -0500

    Formating details, it already compiles

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
-setup_args = generate_distutils_setup(packages=["wifi_sensor"], scripts=["bin/wifi_sensor"], package_dir={"": "src"}, requires=["std_msgs", "rospy"])
+setup_args = generate_distutils_setup(
+    packages=["wifi_sensor"],
+    scripts=["bin/wifi_sensor"],
+    package_dir={"": "src"},
+    requires=["std_msgs", "rospy"])
 
 setup(**setup_args)
